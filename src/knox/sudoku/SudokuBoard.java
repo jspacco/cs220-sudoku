fb72cb574163d59ea54ddfb70b735d7d0b025b45
Jaime Spacco <jspacco@knox.edu>
2021-05-27 20:21:51 -0500

    added grid of jbuttons

--- src/knox/sudoku/SudokuBoard.java
+++ src/knox/sudoku/SudokuBoard.java
@@ -1,26 +1,22 @@
 package knox.sudoku;
-import java.awt.BasicStroke;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.WindowConstants;
 
 
 public class SudokuBoard extends JFrame {
@@ -35,49 +31,49 @@ public class SudokuBoard extends JFrame {
     private int numRows = 9;
     private int numCols = 9;
     
-    private boolean highlight = false;
-    private int highlightRow = 0;
-    private int highlightCol = 0;
-    
     private int width = DOUBLE_MARGIN_SIZE + squareSize * numCols;    		
     private int height = DOUBLE_MARGIN_SIZE + squareSize * numRows;    		
     
     private JPanel canvas;
     private JMenuBar menuBar;
+    private JButton[][] buttons = new JButton[numRows][numCols];
     
-	private void drawGrid(Graphics2D g) {
-		g.setColor(Color.BLACK);
-		Font font = new Font("Verdana", Font.BOLD, 40);
-		g.setFont(font);
-
-        for (int r = 0; r < numRows; r++) {
-            for (int c = 0; c < numCols; c++) {
-            	int x = c * squareSize + MARGIN_SIZE;
-            	int y = r * squareSize + MARGIN_SIZE;
-            	
-            	String num = sudoku.get(r, c) + "";
-            	if (num.equals("0")) {
-            		num = "";
-            	}
-
-            	g.drawString(num,
-                		x + squareSize/3, 
-                		y + 2*squareSize/3);
-
-                g.drawRect(x, y, squareSize, squareSize);
-                
-                // TODO: 3x3 grid squares
-                if (r % 3 == 0 && c % 3 == 0) {
-                	g.setStroke(new BasicStroke(3.0f));
-                	g.drawRect(x, y, squareSize * 3, squareSize * 3);
-                	g.setStroke(new BasicStroke(1.0f));
-                }
-              
-            }
-        }
-        
+    private class ButtonListener implements ActionListener {
+    	public final int row;
+    	public final int col;
+    	ButtonListener(int row, int col){
+    		this.row = row;
+    		this.col = col;
+    	}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.out.printf("row %d, col %d\n", row, col);
+			JButton button = (JButton)e.getSource();
+			// TODO: cycle through possible values, maybe show button differently as highlight
+			
+			
+			update();
+		}
     }
     
+    private void setText(int row, int col, String text) {
+    	buttons[row][col].setText(text);
+    }
+    
+    private void update() {
+    	for (int row=0; row<numRows; row++) {
+    		for (int col=0; col<numCols; col++) {
+    			int val = sudoku.get(row, col);
+    			if (val == 0) {
+    				setText(row, col, "");
+    			} else {
+    				setText(row, col, val+"");
+    			}
+    		}
+    	}
+    }
+    
+	
     private void createMenuBar() {
     	menuBar = new JMenuBar();
         JMenu menu = new JMenu("Actions");
@@ -108,81 +104,33 @@ public class SudokuBoard extends JFrame {
 		});
     }
     
-    private int getRow(int y) {
-    	return (y - MARGIN_SIZE) / squareSize;
-    }
-    
-    private int getCol(int x) {
-    	return (x - MARGIN_SIZE) / squareSize;
-    }
-    
-    private void createMouseHandlers() {
-    	this.addMouseListener(new MouseAdapter() {
-
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				int row = getRow(e.getY());
-				int col = getCol(e.getX()); 
-				System.out.printf("row=%d, col=%d\n", row, col);
-				if (!sudoku.isBlank(row, col)) {
-					highlight = true;
-					highlightRow = row;
-					highlightCol = col;
-				}
-			}
-    		
-		});
-    }
-    
     public SudokuBoard() {
         sudoku = new Sudoku();
+        sudoku.load("easy1.txt");
         
         setTitle("Sudoku!");
 
-        JFrame frame = this;
+        this.setSize(width, height);
         
-        canvas = new JPanel() {
-        	 /* (non-Javadoc)
-             * @see javax.swing.JComponent#getMinimumSize()
-             */
-            public Dimension getMinimumSize() {
-                return new Dimension(width, height);
-            }
-            
-            /* (non-Javadoc)
-             * @see javax.swing.JComponent#getMaximumSize()
-             */
-            public Dimension getMaximumSize() {
-                return new Dimension(width, height);
-            }
-            
-            /* (non-Javadoc)
-             * @see javax.swing.JComponent#getPreferredSize()
-             */
-            public Dimension getPreferredSize() {
-                return new Dimension(width, height);
-            }
-            
-            /* (non-Javadoc)
-             * @see java.awt.Component#isFocusable()
-             */
-            public boolean isFocusable() {
-                return true;
-            }
-
-			@Override
-        	public void paint(Graphics graphics) {
-        		Graphics2D g = (Graphics2D)graphics;
-
-        		drawGrid(g);
-
-        		//frame.setPreferredSize(new Dimension(numRows*squareSize + MARGIN_SIZE, numCols*squareSize + MARGIN_SIZE));
-        		setPreferredSize(new Dimension((numCols+2)*squareSize + 2*MARGIN_SIZE, (numRows+2)*squareSize + 2*MARGIN_SIZE));
-        		frame.pack();
+        canvas = new JPanel();
+        canvas.setLayout(new GridLayout(9, 9));
+        canvas.setPreferredSize(new Dimension(width, height));
+        
+        for (int r=0; r<numRows; r++) {
+        	for (int c=0; c<numCols; c++) {
+        		JButton b = new JButton();
+        		b.setPreferredSize(new Dimension(squareSize, squareSize));
+        		b.addActionListener(new ButtonListener(r, c));
+        		b.setFont(new Font("Verdana", Font.BOLD, 40));
+        		buttons[r][c] = b;
+        		canvas.add(b);
+        		// TODO thicker borders in some places
+        		b.setBorder(BorderFactory.createMatteBorder(3,3,1,1, Color.black));
         	}
-        };
+        }
         
         this.getContentPane().add(canvas, BorderLayout.CENTER);
+        this.setPreferredSize(new Dimension(width, height));
         this.setResizable(false);
         this.pack();
         this.setLocation(100,100);
@@ -190,15 +138,10 @@ public class SudokuBoard extends JFrame {
         
         createMenuBar();
         createKeyboardHandlers();
-        createMouseHandlers();
         
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
-
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         
+        update();
         repaint();
     }
     
