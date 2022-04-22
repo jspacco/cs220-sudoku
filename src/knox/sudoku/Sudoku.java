f3b98b7054694f1ef0d526c1c3b2fa7e97d217f9
khhernandez <khhernandez@knox.edu>
2022-04-21 22:19:12 -0500

    Implemented gameOver Implemented getLegalValues

--- src/knox/sudoku/Sudoku.java
+++ src/knox/sudoku/Sudoku.java
@@ -1,9 +1,12 @@
 package knox.sudoku;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  * 
@@ -22,22 +25,63 @@ public class Sudoku {
 	
 	public int get(int row, int col) {
 		// TODO: check for out of bounds
+		if(row > board.length || col > board[0].length) {
+			throw new IndexOutOfBoundsException(); 
+		}
 		return board[row][col];
 	}
 	
 	public void set(int row, int col, int val) {
 		// TODO: make sure val is legal
+		if(val > 9 || val <= 0) {
+			throw new IndexOutOfBoundsException(val +" is not a legal number! Try again!");
+		}
 		board[row][col] = val;
 	}
 	
 	public boolean isLegal(int row, int col, int val) {
 		// TODO: check if it's legal to put val at row, col
+		for(int i=0; i<board.length; i++) {//going the rows
+			if(board[i][col] == val) 
+				return false;
+			
+			for(int k=0; k<board[0].length; k++) {//going through the cols
+				if(board[row][k] == val) {
+					return false;
+				}
+			}
+		}
+		
 		return true;
 	}
 	
 	public Collection<Integer> getLegalValues(int row, int col) {
 		// TODO: return only the legal values that can be stored at the given row, col
-		return new LinkedList<>();
+		//so we want to check the surrounding parts to see what numbers have already been used
+		/*LinkedList<Integer> legalval = new LinkedList<>();
+		for(int i=0; i<9; i++) {
+			if(this.isLegal(row, col, i)) {
+				legalval.add(i);
+			}
+		}
+		*/
+		//rows and columns
+		Set<Integer> result = new HashSet<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
+		for(int i=0; i<9; i++) {
+			result.remove(board[row][i]);
+			result.remove(board[i][col]);
+		}
+		
+		//remove from the 3x3 grid
+		int rstart = row / 3 * 3;
+		int cstart = col / 3 * 3;
+		for (int r=rstart; r<rstart+3; r++) {
+			for (int c=cstart; c<cstart+3; c++) {
+				result.remove(board[r][c]);
+			}
+		}
+		
+		return result;
 	}
 	
 /**
@@ -113,7 +157,14 @@ etc
 
 	public boolean gameOver() {
 		// TODO check that there are still open spots
-		return false;
+		for(int i=0; i<board.length; i++) {
+			for (int k=0; k<board[0].length; k++) {
+				if(board[i][k] == 0) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 
 	public boolean isBlank(int row, int col) {
---
+++ src/knox/sudoku/Testing.java
@@ -0,0 +1,14 @@
+package knox.sudoku;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		Sudoku sudoku = new Sudoku();
+		sudoku.load("easy1.txt");
+		System.out.println(sudoku);
+		
+		System.out.println(sudoku.getLegalValues(0, 8));
+		System.out.println(sudoku.getLegalValues(6, 8));
+	}
+
+}
